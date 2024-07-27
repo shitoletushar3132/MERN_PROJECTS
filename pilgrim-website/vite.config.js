1819dde32ee2ff629b1204af1e9e15f366e61eda
shitoletushar3132 <shitoletushar3132@gmail.com>
2024-07-27 23:50:10 +0530

    y

--- pilgrim-website/vite.config.js
+++ pilgrim-website/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Base path for the project
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Output directory for the build files
+    rollupOptions: {
+      input: "./index.html", // Update this to the path where your index.html is located
+    },
+  },
+});
